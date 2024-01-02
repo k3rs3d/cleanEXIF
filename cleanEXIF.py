1d0de1789bb89e59fa1a97a8cabe73e0fc2dc0b1
Kern <mccoolname@gmail.com>
2024-01-02 13:08:16 -0800

    Recursive directory scan

--- cleanEXIF.py
+++ cleanEXIF.py
@@ -1,8 +1,25 @@
 from PIL import Image
+import os
 
-img = Image.open("input.jpg")
-info = img.info
 
-if "exif" in info:
-    del info["exif"]
-img.save("output.jpg", exif=b"")
+def remove_exif(file_path):
+    img = Image.open(file_path)
+    info = img.info
+
+    if "exif" in info:
+        del info["exif"]
+
+    img.save(file_path, exif=b"")
+    print(f"EXIF data removed for {file_path} and saved")
+
+
+def process_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(('.bmp', '.png', '.jpg', '.jpeg', '.gif')):
+                file_path = os.path.join(root, file)
+                remove_exif(file_path)
+
+
+# Execute
+process_directory("images")
